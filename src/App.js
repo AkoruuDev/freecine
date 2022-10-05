2104725d5223fb3079f2c30982d78fd3773e4541
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-05 17:39:05 -0300

    FEAT [Connection]: Create a connect for Components from App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Top from "./components/Top";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+import Films from "./components/Films";
+import MovieTime from "./components/MovieTime";
+import Seats from "./components/Seats";
+import Order from "./components/Order";
+
+export default function App() {
+    return(
+        <>
+            <Top />
+            <Main>
+                <Films />
+                <MovieTime />
+                <Seats />
+                <Order />
+            </Main>
+            <Footer />
+        </>
+    );
+}
---
+++ src/components/MovieTime.js
@@ -0,0 +1,5 @@
+export default function MovieTime() {
+    return(
+        <>MovieTime</>
+    );
+}
