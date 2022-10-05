adec572e22f6b614ae4fe80d5dd43e1f18d7e59a
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-05 17:22:20 -0300

    FEAT [Axios]: Create functions for get and post requests for API on App

---
+++ src/services/Axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const BASE_URL = 'https://mock-api.driven.com.br/api/v8/cineflex';
+
+function getMovies() {
+    const promise = axios.get(`${BASE_URL}/movies`);
+    return promise;
+}
+
+function getSessions(MOVIE_ID) {
+    const promise = axios.get(`${BASE_URL}/movies/${MOVIE_ID}/showtimes`);
+    return promise;
+}
+
+function getSeats(SESSION_ID) {
+    const promise = axios.get(`${BASE_URL}/showtimes/${SESSION_ID}/seats`);
+    return promise;
+}
+
+function postRequest(ORDER) {
+    const promise = axios.get(`${BASE_URL}/seats/book-many`, ORDER);
+    return promise;
+}
+
+export { getMovies, getSeats, getSessions, postRequest };
