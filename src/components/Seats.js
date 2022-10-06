c7d1314c9c5c58656ae15ccff2a2da0c984b7336
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-06 20:47:37 -0300

    BAS [Show Seats page]: Create structure for Seats page from App

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getSeats } from "../services/Axios";
+
 export default function Seats() {
+    const [mySeats, setMySeats] = useState([])
+
+    const { sessionID } = useParams();
+
+    console.log(mySeats);
+    useEffect(() => {
+        getSeats(sessionID)
+        .then(res => {
+            setMySeats(res.data.seats)
+        })
+        .catch(() => console.log("Deu ruim"))
+    }, [])
+    
     return(
-        <>Seats</>
+        <div>
+            <h1>Selecione o(s) assento(s)</h1>
+            <div>{mySeats.map(e => <span>{e.name}</span>)}</div>
+            <div>
+                <div>
+                    <div></div>
+                    <p>Selecionado</p>
+                </div>
+                <div>
+                    <div></div>
+                    <p>Disponível</p>
+                </div>
+                <div>
+                    <div></div>
+                    <p>Indisponível</p>
+                </div>
+                <form>
+                    <label htmlFor="inName">Nome do comprador</label>
+                    <input type="text" id="inName" name="inName" placeholder="ex: Maria de Lurdes" />
+                    <label htmlFor="inCPF">CPF do comprador</label>
+                    <input type="text" id="inCPF" name="inCPF" placeholder="ex: 12345678900" />
+                </form>
+            </div>
+        </div>
     );
 }
