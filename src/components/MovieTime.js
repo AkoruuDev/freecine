5a6a849c72d245f4cd8ae2f7e3502edfda3211c5
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-06 20:00:01 -0300

    STYLE [Times Available]: Stylize available times for selected movie on App

--- src/components/MovieTime.js
+++ src/components/MovieTime.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { getSessions } from "../services/Axios";
 
 function Time({ time }) {
     return(
         <div>
-            <>{time.weekday} - {time.date}</>
-            <>{time.showtimes?.map(hour => (<div key={hour.id}>{hour.name}</div>))}</>
+            <Info>{time.weekday} - {time.date}</Info>
+            <ButtonBox>{time.showtimes?.map(hour => (<Button key={hour.id}>{hour.name}</Button>))}</ButtonBox>
         </div>
     )
 }
@@ -25,9 +26,64 @@ export default function MovieTime() {
         .catch(console.log("deu ruim"));
     }, [])
     return(
-        <div>
-            <h1>Selecione o Horario</h1>
-            <>{movieTime?.map(time => <Time key={time.id} time={time} />)}</>
-        </div>
+        <Container>
+            <Title>Selecione o Horario</Title>
+            <Box>{movieTime?.map(time => <Time key={time.id} time={time} />)}</Box>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    padding-top: 110px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Title = styled.h1`
+    font-weight: 700;
+    font-size: 26px;
+    color: #293845;
+`
+
+const Box = styled.div`
+    margin-top: 15px;
+    width: 90vw;
+    height: 60vh;
+
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`
+
+const Info = styled.h2`
+    font-size: 18px;
+    margin: 10px 0px 5px 8px;
+    color: #293845;
+`
+
+const ButtonBox = styled.div`
+    display: flex;
+`
+
+const Button = styled.div`
+    width: 100px;
+    height: 30px;
+    margin: 8px;
+    border-radius: 5px;
+    background-color: #E8833A;
+    color: #FAFAFA;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+        cursor: pointer;
+        box-shadow: 0 4px 10px 4px gray;
+    }
+`
