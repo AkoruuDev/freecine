4a980deea0aef34b5cc02bd1a6f47ddb08662f88
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-06 19:42:30 -0300

    BAS [Show Times]: Create a fnction for show times available from movie selected from App

--- src/components/MovieTime.js
+++ src/components/MovieTime.js
@@ -1,5 +1,33 @@
-export default function MovieTime() {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getSessions } from "../services/Axios";
+
+function Time({ time }) {
     return(
-        <>MovieTime</>
+        <div>
+            <>{time.weekday} - {time.date}</>
+            <>{time.showtimes?.map(hour => (<div key={hour.id}>{hour.name}</div>))}</>
+        </div>
+    )
+}
+
+export default function MovieTime() {
+    const [movieTime, setMovieTime] = useState([])
+
+    const { movieID } = useParams();
+
+    useEffect(() => {
+        getSessions(movieID)
+        .then(res => {
+            console.log(res.data.days);
+            setMovieTime(res.data.days);
+        })
+        .catch(console.log("deu ruim"));
+    }, [])
+    return(
+        <div>
+            <h1>Selecione o Horario</h1>
+            <>{movieTime?.map(time => <Time key={time.id} time={time} />)}</>
+        </div>
     );
 }
